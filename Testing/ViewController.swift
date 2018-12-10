b70c1af4cf614abec5be96c250beee5159c47504
Uzair Tariq <uzair.tariq@binexsolutions.com>
2018-12-10 18:05:41 +0500

    Initial Project Setup with fastlane

---
+++ Testing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Testing
+//
+//  Created by Binex Solutions on 10/12/2018.
+//  Copyright © 2018 Binex Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
